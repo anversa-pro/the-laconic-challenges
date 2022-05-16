6f7f2979744b0332294a108c26ec3aac34546151
Angela Vergara <angelavergaras@gmail.com>
2022-05-15 23:53:54 -0500

    [CREATE] Data type file

---
+++ hacker-rank/python_30-days-of-code/day-1.py
@@ -0,0 +1,27 @@
+# Day 1
+
+# Given variables
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+# --- No need in python!
+
+# Read and save an integer, double, and String to your variables.
+new_integer = int(input())
+new_float = float(input())
+new_str = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + new_integer)
+
+# Print the sum of the double variables on a new line.
+print(f'{d + new_float:.1f}')
+
+# Concatenate and print the String variables on a new line
+print(s + new_str)
+
+# The 's' variable above should be printed first.
+
+
